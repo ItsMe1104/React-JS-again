a00fdffce54fe1d9e8dedeb251d1da4672dc57af
Hrithik <hey.its.me.hrithik@gmail.com>
2024-03-17 01:09:06 +0530

    Added files for Chapter 2

---
+++ 02_Igniting our App/App.js
@@ -0,0 +1,172 @@
+
+//create a div element using JS 
+const div_ele = document.createElement("div");
+
+//Attach the div in body
+document.body.appendChild(div_ele);
+
+//creating a React element of type "h1"
+const heading = React.createElement("h1", { id: "heading" }, "Hello World From React 2");
+
+//Creating root of virtual DOM
+const root = ReactDOM.createRoot(div_ele);
+
+//rendering the heading inside root
+root.render(heading);
+
+
+
+
+
+//Practice with nested structures
+
+// 1) Children
+/*
+  <div id = "parent">
+       <h1> Hello World <h1>
+    </div>
+*/
+
+//children can be passed as a new React.createElement() in the third parameter of its parent
+
+const parent = React.createElement(
+  "div",
+  {
+    id: "parent"
+  },
+  React.createElement(
+    "h1",
+    {
+      id: "heading"
+    },
+    "Hello World from React 2"
+
+  )
+)
+
+
+
+
+// 2) Nested Children
+/*
+<div id = "parent">
+    <div id = "child">
+       <h1> Hello World <h1>
+    </div>
+</div>
+
+//Nesting elements
+
+div(parent)  -->  div(child)  -->  h1
+*/
+
+//Nesting of children can be done by passing the children as new React.createElement() in third parameter of each parent level
+
+const parent2 = React.createElement(
+  "div",
+  {
+    id: "parent"
+  },
+  React.createElement(
+    "div",
+    {
+      id: "child"
+    },
+    React.createElement(
+      "h1",
+      {
+        id: "heading"
+      },
+      "Hello World from React 2"
+    )
+  )
+)
+
+
+
+
+// 3) Siblings
+/*
+<div id = "parent">
+    <div id = "child">
+       <h1> Hello World from h1<h1>
+       <h2> Hello World from h1<h1>
+    </div>
+</div>
+
+
+//Here h1 and h2 are siblings
+*/
+
+//For siblings 
+// --> (content + children ) or 
+// --> (children + children )
+
+//we can pass an array with the siblings order we want, as the third parameter of the parent's React.createElement() function
+
+
+/*
+React.createElement(
+  "a",
+  {b},
+  [sibling1, sibling2]
+)
+
+
+//NOTE :- whenever we pass siblings in an array as third argument, React will give warning to pass keys along with the siblings
+*/
+
+const parent3 = React.createElement
+  (
+    "div",
+    { id: "parent" },
+    React.createElement("div", { id: "child" },
+      [
+        React.createElement("h1", { id: "sibling1" }, "Hello World from React 2")
+        ,
+        React.createElement("h2", { id: "sibling2" }, "Hello World from React 2")
+      ]
+    )
+  )
+
+
+// 4) Parent- Siblings along with child siblings
+
+/*
+<div id = "grandparent">
+    <div id = "parent1">
+       <h1> Hello World from h1<h1>
+       <h2> Hello World from h1<h1>
+    </div>
+
+    <div id = "parent2">
+       <h1> Hello World from h1<h1>
+       <h2> Hello World from h1<h1>
+    </div>
+</div>
+
+*/
+
+
+const grand_parent = React.createElement(
+  "div", { id: "grandparent" },
+  [
+    React.createElement(
+      "div", { id: "parent1" },
+      [
+        React.createElement("h1", {}, "Hello World from h1"),
+
+        React.createElement("h2", {}, "Hello World from h2")
+      ]
+    ),
+
+    React.createElement(
+      "div", { id: "parent2" },
+      [
+        React.createElement("h1", {}, "Hello World from h1"),
+
+        React.createElement("h2", {}, "Hello World from h2")
+      ]
+    ),
+  ]
+)
--- 02_Igniting our App/notes.js
+++ 02_Igniting our App/notes.js
@@ -1,5 +1,24 @@
 //React cannot itself build a fast-production ready app
 //lots of other packages, library required to make our production ready app
+// --> like removing comments, whitespaces, making our app light-weight,no console.log(),  etc
+// minifying the code and bundle as well compressing the code
+// optimising the images
+//code splitting
+
+//Hence we would have to do lots of processing in our local before pushing our app for production
+
+
+// NOTE :- when we use "npx create-react-app", it creates a scaffold for us which gives a basic react app already optimised and ready for production.
+
+
+
+//****************************************************************************************************************************************************************************************** */
+
+// 2) N P M :-
+
+//To build a fast production ready APP, we use many other packages, libraries, code written by others
+//Hence, React makes our app fast to an extent but we need other packages to make it more scalable and fast
+
 
 //npm = node package manager (wrong xxxxxx)
 //npm = everything else except node package manager
@@ -8,4 +27,143 @@
 //npm behind the scenes works as a node package manager
 
 //npm is a standard repository for all the packages
-//Its one of the biggest package manager
+//Its one of the biggest package manager + all the packages, libraries, utilities are hosted over there
+
+// app through "create-react-app" automatically has npm inside in it, hence we dont need to configure it
+
+
+
+
+//***************************************************************************************************************************************************************************************************************************************** */
+
+//Inorder to use raw "npm" and configure it
+
+// i) use command "npm init" and answer the questions
+// for test commands :- type "jest"
+// A new file named package.json file will be created
+
+
+// Package.json :- A configuration file for our npm in json structure
+
+/*
+Why package.json is needed?
+--> npm will manage all the packages which we will install in our system
+--> these packages are also called dependencies (the packages our project is dependent on)
+--> npm will take care of the versions of that package inside package.json
+
+*/
+
+
+
+
+
+
+// ii) Install the bundler (most important dependency)
+
+// --> Our code (html,css,js) files needs to be bundled together, minified , cached, compressed, cleaned before it can be production ready
+// bundler helps us to all these things
+// e.g :- webpack, parcel, vite
+//They have almost the same functionalities but with different algorithms behind the scenes
+
+// create-react-app behind the scenes uses "webpack" bundler
+
+// We are using parcel (its my choice)
+// parcel also comes as a package, hence install it using npm
+
+// use "npm install -D parcel"
+// --> There are always two types of dependencies
+//      a) dev dependencies :- required in development phase
+//      b) normal dependencies :- used in development as well as production also
+
+//since we need to make our code production ready in development phase hence we are installing dev dependencies only
+// --> -D = dev dependencies
+
+
+//hence, now parcel would be installed as a devDependency in package.json along with its version
+
+//
+//        "devDependencies":
+//        {
+//          "parcel": "^2.12.0"
+//        }
+
+
+// ^ (carat) :- automatically updates the dependency in our app on minor upgrade of that dependency
+// ~ (tilde) :- automatically updates the dependency in our app on major upgrade of that dependency
+
+// NOTE: - always it is safe to use ^ (carat) so that the dependency updates level by level through minor upgrades, else if we directly update the dependency on major upgrade, then many things might crash in our app
+
+
+
+
+
+//*********************************************************************************************************************************************************************************************************************************************************** */
+
+
+
+//Package.json   vs   Package-lock.json
+
+//--> When we install parcel, we also get package-lock.json
+
+//package.json :- a configuration of npm
+
+//package-lock.json :- keeps a track of exact version that is being installed for every dependency as inside the package.json due to ^ (carat) the dependencies get updated on the inside on every minor upgrade, but package.json still shows the one time installed version with the carat sign only
+
+//package.json :- approx version due to ^ or ~
+//package-lock.json :- exact version
+
+
+
+
+//******************************************************************************************************************************************************************************* */
+// What  is integrity inside parcel in package-lock.json
+
+// integrity :- a SHA512 hash to make sure whatever dependencies are there in local machine, the same version is there deployed during production
+
+
+
+
+//********************************************************************************************************************************************************************************************************************************************************/
+
+//Node-modules :-
+
+// It is a collection of dependencies
+
+// -> on installing parcel, all the exact code of parcel as well as all the dependencies it required was fetched and put inside node-modules
+
+// -> our project required parcel as a dependency, parcel might have some own dev and normal dependencies like babel, etc -> those dependencies might have other dependencies
+//This is known as transitive dependency
+
+//Every dependency is a separate package in npm
+// -> The code of all these dependencies are present inside node modules
+
+
+//NOTE :- Just like our project has a package.json, every dependency inside node modules has a package.json for itself which contains all the dependencies required for that package
+
+
+
+
+//******************************************************************************************************************************************************************************************************************************************************************************************* */
+
+//NODE MODULES AND GITHUB :-
+
+//Node modules should never be put into github while hoisting our project in the repository
+//Because of two reasons
+// --> node modules is too bulky
+
+
+
+
+//HOW to ignore files from going to production or git:-
+
+// --> Create a file named ".gitignore"
+// --> put the location of all files we want to ignore wrt the location of  ".gitignore" file
+
+//e.g :- Inside .gitignore
+// --> /node_modules
+
+
+//********************************************************************************************************************************************************* */
+// Summary
+// use "npm init"
+// use npm i -D parcel
