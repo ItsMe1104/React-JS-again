05f36ad98cce63f978cd36709451e3556a4a526c
Hrithik <hey.its.me.hrithik@gmail.com>
2024-03-20 01:44:58 +0530

    Updated notes file in 3rd chapter.

--- 03_laying the foundation/notes.js
+++ 03_laying the foundation/notes.js
@@ -65,11 +65,77 @@
 
 
 // 4) JSX :- Javascript XML
+//JSX was developed by Facebook omly
 
 // React Element is just an object and not an HTML element
 // when we render this object in the DOM, it becomes an HTML element
 
 
-//But creating React Element (which at last is converted into html element using render()) is a painful method
+//But creating React Element (which at last is converted into html element using render()) is a painful method due to its syntax
+
+//Hence we, can create a React Element directly using JSX instead of using traditional React.createElement()
+
+//React can be written without JSX in traditional way but JSX makes life more easy
 
 
+//Syntax :-
+const jsxHeading = <h1 id="heading"> React Heading using jsx 🚀🚀</h1>
+
+//The above tag is not html but JSX
+//It is not html inside JS
+// JSX is not HTML but an HTML like syntax
+
+// JSX is not converted into HTML element but into a React element instead
+console.log(jsxHeading);
+
+//While execution it is converted to a React element which in turn is converted to an object
+// Now this object is converted to HTMl element using render()
+
+
+// JSX (using Babel) --> React element --> JS object (using React algo) --> HTML element (using render())
+
+
+
+
+
+//WHY JSX :-
+// We write code for humans or machines?
+// -->  Both
+// --> First for humans then for machines
+// --> First it must be understoodby developers and then by machines, else we would have used binary.
+// --> Hence, JSX made the life of humans easy
+
+
+
+// NOTES :-
+// JSX is not a valid pure JS as JS engine doesnt understand JSX (use developer console any JSX and )
+// JS engine understands ECMA script (standard pure JS)
+
+
+
+//***************************************************************************************************************************************************************************************************** */
+
+
+//5) JSX till BROWSER
+
+//--> JSX is transpiled before it reaches the Browser using Babel
+//--> Babel is bydefault installed by parcel.
+// --> Babel transpiles the JSX code into the JS object which represents the React element as soon as we save the file
+
+//Babel :- An open-source javascript compiler or transpiler
+
+
+//Earlier :-
+
+//React.createElement ==> React element (JS object) ==> render(React element) ==> HTML element
+
+
+//Using JSX :-
+
+//JSX ==> React.createElement (transpiled using Babel) ==> React element (JS object) ==> render(React element) ==> HTML element
+
+
+
+
+
+// Some other features of Babel: -
