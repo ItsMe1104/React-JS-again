1e9508ae9bb7aed32f84ae0e668e57182883bbc9
Hrithik <hey.its.me.hrithik@gmail.com>
2024-03-29 01:53:06 +0530

    Updated notes3.js file in chapter 4.

--- 04_Show the code/notes3.js
+++ 04_Show the code/notes3.js
@@ -23,3 +23,28 @@ resList.map((restuarant) => (
 //********************************************************************************************************************************************************************************************************************************************************************************************* */
 
 // Why key props :-
+// ==> React optimizes its render cycles
+// ==> When there are components at the same level, they must be given different key props
+// ==> So that if there is a similar new component which came in at the same level at any position among them (first,last or in middle), React could uniquely identify the newly created component, else
+// ==> React would have to re render all the components again inside that container
+// ==> if we pass key props in the components, then React could uniqely identify the new component as well as the position it needs to be put in, hence it will just render the new component only
+
+
+
+
+//Unique Keys which can be used:-
+// a) Use unique id's from JSON array passed in every object
+// b) Use array indexes where the second parmeter of map(), gives the index, where this index keeps on increasing at every iteration of map
+
+resList.map((item, index) => {
+  return <RestaurantCard key={index} resData={item} />
+})
+
+//NOT RECOMMENDED :- Since there are major issues using array/ map indexes as keys, hence React itself warns us about it,
+//Hence use it as a last resort
+//Problem :- Index as a key is an anti-pattern
+
+// Link :-
+
+
+// Not using keys (not acceptable) <<<< indexes as key <<<<<<<< unique ID (Best practice)
