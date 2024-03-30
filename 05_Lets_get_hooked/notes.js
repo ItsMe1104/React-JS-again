a94e72bf53f1b5aae9c86b34ba547e4549f17143
Hrithik <hey.its.me.hrithik@gmail.com>
2024-03-30 14:23:11 +0530

    Added files for chapter 5 "Let's Get Hooked".

---
+++ 05_Lets_get_hooked/notes.js
@@ -0,0 +1,14 @@
+//Everything React or any framework can do, we can do it using normal HTML, CSS, JS also.
+
+//Why React?
+//It makes our developer experience easy
+//It makes us write less code and do more on the webpage
+//React gives us superpowers so that we can build large production ready-applications
+//React makes our coding experience very fast and also optimizes behind the scenes in the web page
+//Thee superpowers are React hooks
+
+
+
+//1) Industry practices :-
+// a) Best practice is to make different files for different components
+// b) Make a src folder and keep all your components inside it including App.js (Don't forget to change the path inside script tag of index.html)
